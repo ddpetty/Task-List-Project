8d4082c51b82fbd41c2c21278a76bc536ba61a92
ddpetty <dionne.petty@gmail.com>
2017-12-27 16:14:16 -0500

    Exchanged alert with Materialize toast and used Babel to transpile JS for functionality in older browsers/IE.

--- ES6app.js
+++ ES6app.js
@@ -1,11 +1,9 @@
-'use strict';
-
 //Define UI Variables
-var form = document.querySelector('#task-form'),
-    taskList = document.querySelector('.collection'),
-    clearBtn = document.querySelector('.clear-tasks'),
-    filter = document.querySelector('#filter'),
-    taskInput = document.querySelector('#task');
+const form = document.querySelector('#task-form'),
+      taskList = document.querySelector('.collection'),
+      clearBtn = document.querySelector('.clear-tasks'),
+      filter = document.querySelector('#filter'),
+      taskInput = document.querySelector('#task');
 
 // Load all event listeners
 loadEventListeners();
@@ -15,7 +13,7 @@ function loadEventListeners() {
   //DOM load event 
   document.addEventListener('DOMContentLoaded', getTasks);
   // Add Task event
-  form.addEventListener('submit', addTask);
+  form.addEventListener('submit', addTask); 
   //remove task event 
   taskList.addEventListener('click', removeTask);
   // Clear all tasks event
@@ -26,22 +24,22 @@ function loadEventListeners() {
 
 // Get tasks from local Storage
 function getTasks() {
-  var tasks = void 0;
+  let tasks;
   //Checks to see if anything is in local storage
-  if (localStorage.getItem('tasks') === null) {
+  if(localStorage.getItem('tasks') === null) {
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks')); // local storage can only store strings
   }
-  tasks.forEach(function (task) {
+  tasks.forEach(function(task){
     //Create li element
-    var li = document.createElement('li');
+    const li = document.createElement('li');
     // Add class (Materialize specific collection-item class)
     li.className = 'collection-item';
     //Create text node and append to li
     li.appendChild(document.createTextNode(task));
     // Create new link element
-    var link = document.createElement('a');
+    const link = document.createElement('a');
     //Add class
     link.className = 'delete-item secondary-content';
     // Add icon html
@@ -56,19 +54,19 @@ function getTasks() {
 //Add Task to DOM & Local Storage
 function addTask(e) {
   //Checks empty submissions and spacebar submissions
-  if (taskInput.value === '' || taskInput.value.match(/\S/) === null) {
-    // Materialize.toast(message, displayLength, className, completeCallback);
-    Materialize.toast('Please, add a task.', 4000);
+  if(taskInput.value === '' || taskInput.value.match(/\S/) === null) {
+     // Materialize.toast(message, displayLength, className, completeCallback);
+    Materialize.toast('Please, add a task.', 4000)
     e.preventDefault();
   } else {
     //Create li element
-    var li = document.createElement('li');
+    const li = document.createElement('li');
     // Add class (Materialize specific collection-item class)
     li.className = 'collection-item';
     //Create text node and append to li
     li.appendChild(document.createTextNode(taskInput.value));
     // Create new link element
-    var link = document.createElement('a');
+    const link = document.createElement('a');
     //Add class
     link.className = 'delete-item secondary-content';
     // Add icon html
@@ -83,14 +81,14 @@ function addTask(e) {
     taskInput.value = '';
     // Prevents default form submit behavior
     e.preventDefault();
-  }
+  } 
 }
 
 //Store Task in local storage function
 function storeTaskInLocalStorage(task) {
-  var tasks = void 0;
+  let tasks;
   //Checks to see if anything is in local storage
-  if (localStorage.getItem('tasks') === null) {
+  if(localStorage.getItem('tasks') === null) {
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks')); // local storage can only store strings
@@ -102,27 +100,27 @@ function storeTaskInLocalStorage(task) {
 //Remove Task function
 function removeTask(e) {
   // Specifically targets the delete icon and then removes entire li
-  if (e.target.parentElement.classList.contains('delete-item')) {
-    if (confirm('Are you sure that you want to delete this task?')) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure that you want to delete this task?')) {
       e.target.parentElement.parentElement.remove();
 
       // Removes from local storage
       removeTaskFromLocalStorage(e.target.parentElement.parentElement);
-    }
+     } 
   }
 }
 // Remove task from local storage function
 function removeTaskFromLocalStorage(taskItem) {
-  var tasks = void 0;
+  let tasks;
   //Checks to see if anything is in local storage
-  if (localStorage.getItem('tasks') === null) {
+  if(localStorage.getItem('tasks') === null) {
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
   }
-  tasks.forEach(function (task, index) {
+  tasks.forEach(function(task, index){
     //check to see if text content matches ls
-    if (taskItem.textContent === task) {
+    if(taskItem.textContent === task) {
       tasks.splice(index, 1);
     }
   });
@@ -131,7 +129,7 @@ function removeTaskFromLocalStorage(taskItem) {
 
 // Clear all tasks function
 function clearTasks() {
-  while (taskList.firstChild) {
+  while(taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
   // Clear from local storage
@@ -145,11 +143,12 @@ function clearTasksFromLocalStorage() {
 
 // Filter Tasks function
 function filterTasks(e) {
-  var text = e.target.value.toLowerCase();
-  document.querySelectorAll('.collection-item').forEach(function (task) {
-    var item = task.firstChild.textContent;
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach
+  (function(task){
+    const item = task.firstChild.textContent;
     // Checks for lower case letters that matches any letter of inputted text that exists
-    if (item.toLowerCase().indexOf(text) !== -1) {
+    if(item.toLowerCase().indexOf(text) !== -1) {
       task.style.display = 'block';
     } else {
       task.style.display = 'none';
